f13223f6b1fc115b6d74a65df5095c3fcff08c61
InSearchChoco <Hamham578@yandex.ru>
2024-10-24 01:04:28 +0300

    Update config_bd.inc.php

--- config_bd.inc.php
+++ config_bd.inc.php
@@ -1,8 +1,4 @@
 <?php
-  // define('BD_HOST', 'localhost');
-  // define('BD_USER', 'homezx33_root');
-  // define('BD_PASSWORD', 'FWalMwcb3mxd');
-  // define('BD_BASE', 'homezx33_root');
   define('BD_HOST', 'localhost');
   define('BD_USER', 'Users');
   define('BD_PASSWORD', '');
